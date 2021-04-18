750633c8c4eb8c633ffae4e69d9ea8ea5891acd2
Carly Daffan <Carly Daffan@DESKTOP-IPTT0EB>
2021-04-18 14:47:39 -0400

    Added comments for each comparator class

--- CovidTracker/src/covidtracker/CompareAlpha.java
+++ CovidTracker/src/covidtracker/CompareAlpha.java
@@ -2,14 +2,22 @@
  * 
  */
 package covidtracker;
+
 import java.util.Comparator;
 
 /**
+ * Comparator for Race names
+ * 
  * @author Carly Daffan
  * @version 4.18.21
  */
 public class CompareAlpha implements Comparator<Race> {
-
+    /**
+     * Returns the name which is closer to the beginning of the alphabet
+     * 
+     * @return int of which Race has the name closer to the beginning of the
+     *         alphabet
+     */
     public int compare(Race left, Race right) {
         return left.getName().compareTo(right.getName());
     }
--- CovidTracker/src/covidtracker/CompareCFR.java
+++ CovidTracker/src/covidtracker/CompareCFR.java
@@ -6,11 +6,16 @@ package covidtracker;
 import java.util.Comparator;
 
 /**
+ * Comparator for CFR numbers
  * @author Carly Daffan
  * @version 4.18.21
  */
 public class CompareCFR implements Comparator<Race> {
-  
+    /**
+     * Returns the highest CFR
+     * 
+     * @return int of which Race has the higher CFR
+     */
     public int compare(Race left, Race right) {
         return (int)(left.getCFR()-right.getCFR());
     }
