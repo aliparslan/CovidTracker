697be41c13b0a453dbb7614eb994238683f4a8e7
Carly Daffan <Carly Daffan@DESKTOP-IPTT0EB>
2021-04-18 14:41:29 -0400

    Fixed formatting and added CompareCFR class

--- CovidTracker/src/covidtracker/CompareCFR.java
+++ CovidTracker/src/covidtracker/CompareCFR.java
@@ -2,15 +2,17 @@
  * 
  */
 package covidtracker;
+
 import java.util.Comparator;
 
 /**
  * @author Carly Daffan
  * @version 4.18.21
  */
-public class CompareAlpha implements Comparator<Race> {
-
+public class CompareCFR implements Comparator<Race> {
+  
     public int compare(Race left, Race right) {
-        return left.getName().compareTo(right.getName());
+        return (int)(left.getCFR()-right.getCFR());
     }
+
 }
