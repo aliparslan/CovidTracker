21b5ae143dfb334cd736bad1ca9de1ebcd36331a
Carly Daffan <Carly Daffan@DESKTOP-IPTT0EB>
2021-04-20 16:35:44 -0400

    Test all pass now

--- CovidTracker/src/covidtracker/RaceTest.java
+++ CovidTracker/src/covidtracker/RaceTest.java
@@ -7,21 +7,19 @@ import student.TestCase;
 
 /**
  * @author Carly Daffan
- *
+ * @version 4.20.21
  */
 public class RaceTest extends TestCase {
     private Race race1;
-    private Race race2;
     private Race race3;
     
     public void setUp() {
         race1 = new Race("White", 50, 10);
-        race2 = new Race("Mexican", 70, 40);
         race3 = new Race("Indian", 150, 110);
     }
     
     public void testAll() {
-        assertEquals(race1.getCFR(), .2);
+        assertEquals(race1.getCFR(), .2, 2);
         assertEquals(race3.getName(), "Indian");
     }
 }
